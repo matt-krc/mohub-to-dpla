89218af4ed3c055cdc52ff224f164823ef897c4c
missouri-hub <Matthew.Krc@stls.frb.org>
2021-06-02 11:04:43 -0500

    a bunch of updates

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ def compile():
     outfn = "./files/ingests/mohub_ingest_{}.json".format(datestr)
     with open(outfn, "w") as outf:
         json.dump(out, outf, indent=4)
+    # additionally, write to jsonl, as DPLA prefers
+    with open(outfn + "l", "w") as outf:
+        for line in out:
+            json.dump(line, outf)
+            outf.write('\n')
     print("Total: {}".format(len(out)))
     print("Wrote ingest file to {}".format(outfn))
 
@@ -49,25 +54,37 @@ def get_previous(institution, outfn):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser()
     parser.add_argument('--institutions', '-i', nargs='*',
-                        help="If argument is set, specifies specific institutions to parse", required=False)
-    parser.add_argument('--ignore_time', '-ig', default=False,
-                        action="store_true", help="If set, ignores whether data already harvested recently")
-    parser.add_argument('--no_csv', '-ncsv', default=False,
-                        action="store_true", help="If set, doesn't generate CSV files.")
+                        help="If argument is set, specifies specific institutions to crawl.", required=False)
+    parser.add_argument('--ignore_time', '-g', default=False,
+                        action="store_true", help="If set, ignores whether data already harvested recently.")
+    parser.add_argument('--csv', '-csv', default=False,
+                        action="store_true", help="If set, generates accompanying CSV files.")
     parser.add_argument('--compile', '-c', default=False,
                         help="If set, compiles all data files into one.", action="store_true")
-    parser.add_argument('--compile_only', '-co', default=False,
+    parser.add_argument('--compile_only', '-o', default=False,
                         help="If set, script doesn't run a crawl, only compiles all data files into one.", action="store_true")
     args = parser.parse_args()
 
-    # infile is a path to a JSON file containing a list of OAI endpoint URLs, along with additional institutional metadata
-    infile = './files/mohub_oai.json'
-
+    # If we only want to compile existing data files into combined file, set this argument.
     if args.compile_only:
         compile()
         return True
+
+    """
+    The main input file is a path to a JSON file containing an array of OAI endpoint URLs, along with additional institutional metadata:
+    {
+        "institution": // the name of the institution,
+        "id": // internal id for institution, used in the mapping functions,
+        "@id_prefix": // a prefix for the item @id field in the output metadata,
+        "url": // url to the root OAI endpoint, or data dump,
+        "metadata_prefix": // metadata prefix for OAI feed, to be used in constructing the OAI query. If set to 'data_dump', URL assumed to be downloaded as-is,
+        "include": // array listing collection names to be included in crawl. If set, only collections listed will be included, otherwise all collections assumed to be included,
+        "exclude": // array listing collection names to be excluded in crawl. If set, all but listed collections will be excluded
+    }
+    """
+    infile = "./files/mohub_oai.json"
 
     with open(infile, "r") as inf:
         data = json.load(inf)
@@ -78,38 +95,42 @@ def main():
         institution = row['institution']
 
         if args.institutions:
-            if row["id"] not in args.institutions:
+            if row['id'] not in args.institutions:
                 continue
 
-        if not args.ignore_time:
+        # In order not to crawl redundantly, by default we skip crawls that have already taken place in the past 24 hours
+        if not args.ignore_time and os.path.exists(f"./files/institutions/{row['id']}.json"):
             now = datetime.now()
             if now-timedelta(hours=24) <= datetime.fromtimestamp(os.path.getmtime(f"./files/institutions/{row['id']}.json")) <= now:
                 print(f"{institution} has been crawled less than 24 hours ago. Continuing")
                 continue
 
         if not url:
-            print("No OAI feed for {}. Using previous ingest data.".format(institution))
+            # TODO: this was used to gather data for Missouri History Museum, which now has a data dump instead.
+            # We can probably delete
+            print(f"No OAI feed for {institution}. Using previous ingest data.")
             get_previous(institution, row["id"] + ".json")
             continue
 
-        # metadata_prefix = OAI().get_metadata_prefix(url)
-        metadata_prefix = row["schema"]
+        metadata_prefix = row["metadata_prefix"]
         print(institution)
         print(url)
-        if not metadata_prefix or not institution:
-            print("A metadata prefix or institution name couldn't be established.")
 
-        if row["include"]:
-            for include_set in row["include"]:
-                metadata = OAI().harvest(url, metadata_prefix, institution, row["id"], row["exclude"], include_set)
-                out.extend(metadata)
-            write_files(out, row["id"])
-            continue
+        # Some institutions have opted to provide a data dump rather than an OAI feed.
+        if metadata_prefix == 'data_dump':
+            metadata = OAI().get_datadump(url)
+        else:
+            if row["include"]:
+                for include_set in row["include"]:
+                    metadata = OAI().harvest(url, metadata_prefix, institution, row["id"], row['@id_prefix'], row["exclude"], include_set)
+                    out.extend(metadata)
+                write_files(out, row["id"])
+                continue
 
-        metadata = OAI().harvest(url, metadata_prefix, institution, row["id"], row["exclude"])
+            metadata = OAI().harvest(url, metadata_prefix, institution, row["id"], row['@id_prefix'], row["exclude"])
         out.extend(metadata)
         write_files(out, row["id"])
-    if not args.no_csv:
+    if args.csv:
         if args.institutions:
             for i in args.institutions:
                 create_csvs(i)
