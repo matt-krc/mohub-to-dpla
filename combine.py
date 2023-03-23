3762af038e5dc45df770989d835ddc4cfb75a0c4
Matt Krc <mattkrc@h1mxk04al1s.lan>
2023-03-23 14:17:04 -0500

    add combine and upload workflow

--- combine.py
+++ combine.py
@@ -1,3 +1,47 @@
-import utils
+from glob import glob
+import json
 
-utils.compile()
+def write_report():
+    data_files = glob("./*_data/*.json")
+    with open("report.txt", "w") as outf:
+        for file in data_files:
+            with open(file, 'r') as inf:
+                data = json.load(inf)
+            skipped = data['skipped']
+            count = data['count']
+            name = data['institution']
+            outf.write(f"# {name}\n")
+            outf.write(f"   - {count} records added\n")
+            outf.write(f"   - {skipped} records skipped\n\n")
+            # for reason, records in data['skipped_errors'].items():
+            #     outf.write(f"       - {reason}: {count(records)}\n\n")
+            inf.close()
+
+def compile():
+    data_files = glob("./*_data/*.json")
+    if len(data_files) < 1:
+        raise Exception("No files found to compile!")
+    print("Compiling...")
+    out = []
+    for file in data_files:
+        print(file)
+        with open(file, "r") as inf:
+            data = json.load(inf)
+        out.extend(data['records'])
+        inf.close()
+    outfn = "mohub_ingest.json"
+    outfn_l = f"{outfn}l"
+    # with open(outfn, "w") as outf:
+    #     json.dump(out, outf, indent=4)
+    # finish by writing to jsonl, as DPLA prefers
+    with open(outfn_l, "w") as outf:
+        for line in out:
+            json.dump(line, outf)
+            outf.write('\n')
+    write_report()
+    print("Total: {}".format(len(out)))
+    print("Wrote ingest file to {}".format(outfn))
+
+
+compile()
+
--- upload_s3.py
+++ upload_s3.py
@@ -1,5 +1,23 @@
-import utils
-import zipfile
+import boto3
+import os
+import progressbar
+from datetime import datetime
 
+def upload_s3(fpath):
+    fn = fpath.split("/")[-1]
+    client = boto3.client('s3',
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_KEY')
+    )
+    statinfo = os.stat(fpath)
+    up_progress = progressbar.progressbar.ProgressBar(maxval=statinfo.st_size)
+    up_progress.start()
+    def upload_progress(chunk):
+        up_progress.update(up_progress.currval + chunk)
+    now = datetime.now().strftime("%Y%m%d")
+    print("starting upload...")
+    res = client.upload_file(fpath, os.getenv('S3_BUCKET'), f"{now}/{fn}", Callback=upload_progress)
+    up_progress.finish()
+    print(f"finished upload to {now}/{fn}")
 
-utils.upload_s3("mohub_ingest.jsonl")
+upload_s3("mohub_ingest/mohub_ingest.jsonl")
